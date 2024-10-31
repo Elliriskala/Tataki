ec9d9d70c4ddce48e70e2b72eb5b5b2d2b5673ef
karripartanen <karripartanen25@gmail.com>
2024-10-31 15:08:11 +0200

    fixed issues in navigation, unified js files

--- Project/src/main.js
+++ Project/src/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('login-btn');
     const registerButton = document.getElementById('register-btn');
@@ -63,4 +62,38 @@ document.addEventListener('DOMContentLoaded', () => {
         // Remove custom validity me
     }); 
     }
+});
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+    const navList = document.querySelector(".nav-list");
+    
+    navList.classList.toggle("active");
+
+    if (navList.classList.contains("active")) {
+        navList.style.height = navList.scrollHeight + "px"; 
+    } else {
+        navList.style.height = "0"; 
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const today = new Date().toISOString().split('T')[0];
+    document.getElementById('date').setAttribute('min', today);
+
+    const detailsElements = document.querySelectorAll('details');
+
+    detailsElements.forEach(details => {
+        const content = details.querySelector('.content');
+        
+        details.addEventListener('toggle', () => {
+            if (details.open) {
+                const contentHeight = content.scrollHeight + 'px';
+                content.style.maxHeight = contentHeight;
+            } else {
+                content.style.maxHeight = '0';
+            }
+        });
+    });
 });
