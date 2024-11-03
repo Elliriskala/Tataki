104c63e7debadf5a5ce0e4454a84b5b60d94ad96
karripartanen <karripartanen25@gmail.com>
2024-11-03 18:06:50 +0200

    added the general functions to utils, fixed responsiveness

--- Project/utils/functions.ts
+++ Project/utils/functions.ts
@@ -32,13 +32,13 @@ const calculateDistance = ( // Calculate distance between two coordinates on a s
     const R = 6371; // Radius of the Earth in km
     const dLat = (lat2 - lat1) * (Math.PI / 180);
     const dLon = (lon2 - lon1) * (Math.PI / 180);
-    const a = 
+    const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(lat1 * (Math.PI / 180)) *
         Math.cos(lat2 * (Math.PI / 180)) *
         Math.sin(dLon / 2) *
         Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return R * c; // Distance in km
   };
 
@@ -57,3 +57,26 @@ const isLoggedIn = () => {
     return token ? true : false;
 }
 
+// Replace HTML content of an element (most likely body for single page applications)
+const replaceHTML = (element: HTMLElement, content: string) => {
+  element.innerHTML = "";
+  element.insertAdjacentHTML('beforeend', content);
+}
+
+// Add content to the end of an element
+const addBeforeEnd = (element: HTMLElement, content: string) => {
+  element.insertAdjacentHTML('beforeend', content);
+}
+
+// format a date string to a more readable format
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+
+  return date.toLocaleDateString('fi-FI', options);
+}
+
+export { fetchData, getUserLocation, calculateDistance, showPassword, isLoggedIn, replaceHTML, addBeforeEnd, formatDate };
---
+++ Project/utils/interfaces.ts
@@ -0,0 +1,10 @@
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+}
+
+export {User};
+
