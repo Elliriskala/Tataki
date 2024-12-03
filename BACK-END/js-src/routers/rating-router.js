6c922535cce0522fb043a9c021337d0a51f838c4
karripartanen <karripartanen25@gmail.com>
2024-12-03 18:16:17 +0200

    added validation to routes

--- BACK-END/js-src/routers/rating-router.js
+++ BACK-END/js-src/routers/rating-router.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { getRestaurantReviews, getRestaurantReviewsByUserId, deleteReview, postReview} from "../controllers/rating-controller.js";
 import authenticateToken from "../middlewares/authentication.js";
+import { body } from "express-validator";
+import { validationErrorHandler } from "../middlewares/error-handlers.js";
 const ratingRouter = express.Router();
 
 ratingRouter.get('/restaurant/', getRestaurantReviews);
 ratingRouter.get('/restaurant/user/:user_id', authenticateToken, getRestaurantReviewsByUserId);
 ratingRouter.delete('/restaurant/:review_id', authenticateToken, deleteReview);
-ratingRouter.post('/restaurant/', postReview);    
+ratingRouter.post('/restaurant/',
+    body('star_rating').isNumeric(),
+    body('review').isString() || body('review').isEmpty(),
+    validationErrorHandler,
+    postReview);    
 export default ratingRouter;
--- BACK-END/js-src/routers/reservation-router.js
+++ BACK-END/js-src/routers/reservation-router.js
@@ -2,14 +2,26 @@ import express from 'express';
 import { getReservations, getReservationById, getReservationsByUserId, validateAndAddReservation, deleteReservationById, modifyReservationById } from '../controllers/reservation-controllers.js';
 import authenticateToken from '../middlewares/authentication.js';
 import { validateAvailability } from '../controllers/reservation-controllers.js';
+import { body } from 'express-validator';
+import { validationErrorHandler } from '../middlewares/error-handlers.js';
+
 const reservationRouter = express.Router();
 reservationRouter
     .get('/', getReservations)
     .get('/times', validateAvailability)
-    .post('/', validateAndAddReservation);
+    .post('/',
+        body('user_id').isNumeric(),
+        body('reservation_date').isString(),
+        body('reservation_time').isString(),
+        body('guests').isNumeric(),
+        body('email').isEmail(),
+        body('full_name').isString(),
+        body('phone_number').isString(),
+        validationErrorHandler,
+        validateAndAddReservation);
 
 
-reservationRouter.get('/:reservation_id', authenticateToken, getReservationById);
+reservationRouter.get('/id/:reservation_id', authenticateToken, getReservationById);
 
 
 reservationRouter.get('/user/:user_id', authenticateToken, getReservationsByUserId);
