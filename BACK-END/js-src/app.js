3f647cc2a2f138aa633bfa5ecda451d19326a0dd
karripartanen <karripartanen25@gmail.com>
2024-12-08 18:22:51 +0200

    documentation configs

--- BACK-END/js-src/app.js
+++ BACK-END/js-src/app.js
@@ -41,11 +41,6 @@ const __dirname = path.dirname(__filename);
 // Serve static files from the "public" directory 
 app.use(express.static(path.join(__dirname, frontEndPath)));
 
-// render api documentation:
-
-// render jsdoc documentation:
-
-
 // Define routes for known pages
 app.get('/about', (_req, res) => {
     res.sendFile(path.join(__dirname, frontEndPath, 'about.html'));
