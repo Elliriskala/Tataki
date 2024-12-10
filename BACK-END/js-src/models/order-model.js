da59a18b14b55d72a07d7347ea3a29110c3125dd
karripar <karripartanen25@gmail.com>
2024-12-10 15:24:20 +0200

    fix

--- BACK-END/js-src/models/order-model.js
+++ BACK-END/js-src/models/order-model.js
@@ -266,7 +266,7 @@ const createOrder = async (
     const order_status_id = statusResult[0].status_id;
     // insert order into orders table
     const orderSql = `
-        INSERT INTO orders (user_id, customer_name, total_price, order_type, status_id, is_delivery, general_comment) 
+        INSERT INTO Orders (user_id, customer_name, total_price, order_type, status_id, is_delivery, general_comment) 
          VALUES (?, ?, ?, ?, ?, ?, ?)
     `;
 
@@ -298,7 +298,7 @@ const createOrder = async (
 
     // insert order items into order_items table
     const orderItemsSql =
-      'INSERT INTO orderitems (order_id, menu_id, item_quantity) VALUES ?';
+      'INSERT INTO OrderItems (order_id, menu_id, item_quantity) VALUES ?';
 
     const orderItemsValues = order_items.map((item) => [
       orderId,
@@ -383,7 +383,7 @@ const createOrder = async (
 const updateOrderStatus = async (order_id, order_status) => {
   try {
     const sql = `
-    UPDATE orders 
+    UPDATE Orders 
     SET status_id = (SELECT status_id FROM OrderStatus WHERE status_name = ?) 
     WHERE order_id = ?
 `;
@@ -398,25 +398,25 @@ const updateOrderStatus = async (order_id, order_status) => {
     // fetch the updated order details
     const updatedOrderSql = `
                 SELECT 
-                    orders.order_id, 
-                    orders.total_price, 
+                    Orders.order_id, 
+                    Orders.total_price, 
                     OrderStatus.status_name AS order_status,
-                    orders.customer_name, 
-                    orders.created_at, 
-                    orders.general_comment, 
+                    Orders.customer_name, 
+                    Orders.created_at, 
+                    Orders.general_comment, 
                     GROUP_CONCAT(CONCAT(OrderItems.item_quantity, ' x ', Menus.course_name) ORDER BY OrderItems.order_item_id) AS order_items,
-                    orders.is_delivery, 
+                    Orders.is_delivery, 
                     DeliveryDetails.delivery_address, 
                     DeliveryDetails.city, 
                     DeliveryDetails.delivery_instructions
-                FROM orders
-                JOIN OrderStatus ON orders.status_id = OrderStatus.status_id
-                LEFT JOIN OrderItems ON orders.order_id = OrderItems.order_id
+                FROM Orders
+                JOIN OrderStatus ON Orders.status_id = OrderStatus.status_id
+                LEFT JOIN OrderItems ON Orders.order_id = OrderItems.order_id
                 LEFT JOIN Menus ON OrderItems.menu_id = Menus.menu_id
-                LEFT JOIN DeliveryDetails ON orders.order_id = DeliveryDetails.order_id
-                WHERE orders.order_id = ?
-                GROUP BY orders.order_id
-                ORDER BY orders.created_at DESC;
+                LEFT JOIN DeliveryDetails ON Orders.order_id = DeliveryDetails.order_id
+                WHERE Orders.order_id = ?
+                GROUP BY Orders.order_id
+                ORDER BY Orders.created_at DESC;
       `;
     const [updatedOrderResult] = await promisePool.query(updatedOrderSql, [
       order_id,
